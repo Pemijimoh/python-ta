86ab0f27c526eae814e6774fbcdd7d590fea4ee1
Opemi Jimoh <60929866+Pemijimoh@users.noreply.github.com>
2022-06-19 08:00:44 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,6 +4,24 @@ import os
 
 def prime(s):
     # your code goes here
+    # Function to check if a number is a prime number
+
+    s = int(sys.argv[1])
+    if (s == 1):
+        print(s, "is not really a prime number since prime numbers are greater than 1")
+    elif (s == 2) or (s == 3):
+        print(s, "is a prime number")
+    elif (s % 3 == 0):
+        print(s, "is not a prime number")
+
+    else:
+        for num in range(2, s):
+            if (s % num == 0):
+                print(s, "is not a prime number")
+                break
+            else:
+                print(s, "is a prime number")
+                break
 
 def solution(s):
     return prime(s)
